8fbf3d20012b592f7025475c79ab0976fe748796
kimminseo <me@msmscomet.dev>
2026-04-13 16:15:36 +0900

    Setting for deployment

--- app.config.ts
+++ app.config.ts
@@ -2,5 +2,14 @@ export default defineAppConfig({
   ui: {
     primary: 'customPrimary',
     gray: 'customGray',
-  }
+  },
+
+  privateRuntimeConfig: {
+    databaseUrl: process.env.DATABASE_URL,
+    databasePort: process.env.DATABASE_PORT,
+    databaseUser: process.env.DATABASE_USER,
+    databasePassword: process.env.DATABASE_PASSWORD,
+    databaseName: process.env.DATABASE_NAME,
+    redisDsn: process.env.REDIS_DSN,
+  },
 })
