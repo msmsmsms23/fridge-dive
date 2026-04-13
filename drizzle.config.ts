0ed6f6b79267f7567b2178d79a301bc851e1e258
kimminseo <me@msmscomet.dev>
2026-04-13 17:16:27 +0900

    Setting for deployment

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'drizzle-kit';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
 
 export default defineConfig({
-  schema: './server/db',
-  out: './migrations',
+  schema: './server/db/schema.ts',
+  out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    url: process.env.DATABASE_URL!,
   },
 });
