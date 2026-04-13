e17c31818e7f0aeb477dec30b65fc2f0ac99f69d
kimminseo <me@msmscomet.dev>
2026-04-13 17:16:27 +0900

    Setting for deployment

--- server/utils/database.js
+++ server/utils/database.js
@@ -2,7 +2,7 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from '../db/schema';
 
-const client = postgres(process.env.DATABASE_URL!, {
+const client = postgres(process.env.DATABASE_URL, {
   ssl: 'require',
 });
 const db = drizzle(client, { schema });
